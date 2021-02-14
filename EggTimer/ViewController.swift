8176cae563b1b8891e5264a5a26717135b546f69
Ethan Smith <edhsmith@icloud.com>
2021-02-14 21:16:31 +1300

    Added basic countdown timer with print functionality.

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -9,14 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var counter = 60
+    let eggTimes = ["Soft": 300, "Medium": 420, "Hard": 720]
     
-    let eggTimes = ["Soft": 5, "Medium": 7, "Hard": 12]
-
+    @objc func startTimer() {
+        print("It works!")
+    }
     
     @IBAction func hardnessSelected(_ sender: UIButton) {
         let hardness = sender.currentTitle!
-        print(eggTimes[hardness]!)
+        counter = eggTimes[hardness]!
+
+        Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
     }
     
+    @objc func updateCounter() {
+        //example functionality
+        if counter > 0 {
+            print("\(counter) seconds.")
+            counter -= 1
+        }
+    }
 
 }
