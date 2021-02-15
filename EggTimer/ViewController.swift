c17e2eb0335cea16990e365c582ee08458314bba
Ethan Smith <edhsmith@icloud.com>
2021-02-15 14:15:43 +1300

    Completed countdown functionality. Added tracking progress bar. Verified countdown works as intended, tested for bugs.

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,26 +10,48 @@ import UIKit
 
 class ViewController: UIViewController {
     var counter = 60
-    let eggTimes = ["Soft": 300, "Medium": 420, "Hard": 720]
-    
-    @objc func startTimer() {
-        print("It works!")
-    }
+    var countdownRatio: Double = 1.0
+    var currentCountdown: Double = 1.0
+    var timer = Timer()
+    let eggTimes = ["Soft": 3, "Medium": 33, "Hard": 720]
     
     @IBAction func hardnessSelected(_ sender: UIButton) {
+        // Invalidated any active timers
+        timer.invalidate()
+        
+        // Reset environment and setup timer settings
         let hardness = sender.currentTitle!
+        currentCountdown = 1.0
         counter = eggTimes[hardness]!
-
-        Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
+        countdownTimerLabel.text = "\(counter) seconds."
+        countdownRatio = 1.0 / Double(counter)
+        countdownProgressBar.progress = 1
+        
+        // Start new timer with new settings
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
     }
     
     @objc func updateCounter() {
-        //example functionality
+        // Continue if timer still active
         if counter > 0 {
-            countdownTimerLabel.text = "\(counter) seconds."
+            // Continue countdown, update variables
+            currentCountdown -= countdownRatio
             counter -= 1
+            // Set label and progress bar
+            countdownTimerLabel.text = "\(counter) seconds."
+            if (Float(currentCountdown - countdownRatio) >= 0) {
+                countdownProgressBar.progress = Float(currentCountdown)
+            } else {
+                countdownProgressBar.progress = 0.0
+            }
+        } else {
+            // If timer complete, finish countdown
+            timer.invalidate()
+            countdownTimerLabel.text = "Done!"
         }
     }
 
+    @IBOutlet weak var countdownProgressBar: UIProgressView!
+    
     @IBOutlet weak var countdownTimerLabel: UILabel!
 }
